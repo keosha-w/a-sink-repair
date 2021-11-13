12b7df7927f3107cf1c8c11899bac8d29aa84346
keosha-w <keosha.watkins@asurion.com>
2021-11-13 11:36:31 -0600

    Tiny changes in formatting.

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -4,14 +4,14 @@ const convertRequestToListElement = (request) => {
     const plumbers = getPlumbers()
     return `<li>${request.description}
 
-<select class="plumbers" id="plumbers">
-<option value="">Choose</option>
-${plumbers.map(
-        plumber => {
-            return `<option value="${request.id}--${plumber.id}">${plumber.name}</option>`
-        }
-    ).join("")
-        }
+        <select class="plumbers" id="plumbers">
+            <option value="">Choose</option>
+                ${plumbers.map(
+                    plumber => {
+                        return `<option value="${request.id}--${plumber.id}">${plumber.name}</option>`
+                     }
+                ).join("")
+             }
 </select>
         <button class="request__delete"
                 id="request--${request.id}">
